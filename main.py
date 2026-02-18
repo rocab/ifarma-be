7f3b018c40b355bc47717bb00d54537bfecdc059
tsyplenkov <ghrjwk@gmail.com>
2026-02-18 23:51:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from api.endpoints import router  
 
 app = FastAPI(title="iPharma BE", version="0.1.0")
 
-# c какого порта Flutter?
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(router)  
 
 @app.get("/health")
 async def health_check():
